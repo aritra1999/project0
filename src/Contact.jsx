98abeb93bacb86cb87c3b6f18ab13914c6ba99a5
Aritra Mondal <mondalaritra002@gmail.com>
2021-11-29 11:48:04 +0530

    Added contact form on mobile view, fixed layout.

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,15 +6,14 @@ import { BiPencil } from "react-icons/bi";
 const Contact = () => {
     const [sendMessage, setsendMessage] = React.useState(false);
     return (
-        <>
-            <div className="absolute bottom-4 right-4 z-40 bg-white">
-                <button className="border border-gray-200 text-indigo-600 hover:shadow-md py-4 pl-4 pr-6 rounded-full flex items-center font-semibold" onClick={() => setsendMessage(true)}>
+        <div>
+            <div className="fixed bottom-4 -right-8 z-30">
+                <button className="border border-gray-200 bg-white text-indigo-600 hover:shadow-md py-4 pl-4 pr-6 rounded-full flex items-center font-semibold" onClick={() => setsendMessage(true)}>
                     <BiPencil className="mr-4 h-6 w-6 font-bold transform -rotate-6" />
-                    Contect Me
                 </button>
             </div>
             { sendMessage ? (
-            <div className="fixed right-0 bottom-0 z-50 w-full bg-white border border-gray-200">
+            <div className="fixed right-0 bottom-0 z-50 w-full bg-white rounded-t-lg">
                 <div className="flex justify-between py-3 px-6 bg-gray-800 text-white rounded-t-lg text-sm items-center">
                     <div>Contact Me</div>
                     <div className="cursor-pointer" onClick={() => setsendMessage(false)}>
@@ -36,7 +35,7 @@ const Contact = () => {
                 </button>
             </div>
             ) : null}
-        </>
+        </div>
     );
 }
 
